2b02c789cf13d5105891d5dff789fe8c067317c1
Lucas Fritzke <86270203+lucasfritzke@users.noreply.github.com>
2024-10-27 20:29:59 -0300

    components

--- blu-motos/src/app/app.routes.ts
+++ blu-motos/src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './header/header.component';
+
 
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'header', component: HeaderComponent },  // Apenas exemplo para outra rota
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redireciona para o login ao acessar a raiz
-  { path: '**', redirectTo: '/login' } // Redireciona para login em caso de rota não encontrada
+
 ];
---
+++ blu-motos/src/app/components/handler/handler.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-handler',
+  standalone: true,
+  imports: [],
+  templateUrl: './handler.component.html',
+  styleUrl: './handler.component.css'
+})
+export class HandlerComponent {
+
+}
