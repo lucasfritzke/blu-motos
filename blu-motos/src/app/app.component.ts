1c349aee73ab1d92605c7e2ce7d3cc3088573b2f
Lucas Fritzke <86270203+lucasfritzke@users.noreply.github.com>
2024-11-02 15:28:52 -0300

    Adicionando componenetes

--- blu-motos/src/app/app.component.ts
+++ blu-motos/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HandlerComponent } from './components/handler/handler.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    HandlerComponent,
+    FooterComponent,
+    MainComponent,
+    NavComponent,
+    InicioComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'blu-motos';
--- blu-motos/src/app/app.routes.ts
+++ blu-motos/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
-
-
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MainComponent } from './components/main/main.component';
+import { CadastrarComponent } from './components/clientes/cadastrar/cadastrar.component';
 
 
 export const routes: Routes = [
+{
+  path: 'inicio', component: InicioComponent
+},
+{path: '', redirectTo: "inicio", pathMatch: 'full'},
+{path: 'cadastrarClientes', component: CadastrarComponent}
 
 ];
